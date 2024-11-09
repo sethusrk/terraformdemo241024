c19faa995e8e90762cf1a6f7a7b3b401035e4851
sethusrk <sethusrk285@gmail.com>
2024-11-09 13:05:54 +0530

    error rectified

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "firstserver" {
 ami           = data.aws_ami.amlin.id
 instance_type = var.instance_type
-security_groups = data.aws_security_group.terraformdemo.id
+vpc_security_group_ids = [data.aws_security_group.terraformdemo.id]
 
   tags = {
     Name = "HelloWorld"
