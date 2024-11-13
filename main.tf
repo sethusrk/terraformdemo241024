bad6c5824b5533a11ed55aa91d92f6c9328d2b80
sethusrk <sethusrk285@gmail.com>
2024-11-13 19:27:38 +0530

    subnet removed

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "ec2instance" {
   version = "5.7.1"
   ami           = data.aws_ami.amlin.id
   instance_type = var.instance_type
-  subnet_id = module.vpc.private_subnets
+  #subnet_id = module.vpc.private_subnets
   vpc_security_group_ids = [module.security-group.security_group_id]
 
     tags = {
