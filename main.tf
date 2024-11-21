46ac152eb2198bf65ac841bc1ef8914dc49aae15
sethusrk <sethusrk285@gmail.com>
2024-11-21 18:25:26 +0530

    2 pvt subnets added

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "eks" {
   cluster_version = "1.31"
 
   vpc_id          = module.vpc.vpc_id
-  subnet_ids      = [var.private_subnet_id]
+  subnet_ids      = var.private_subnet_id
 
   eks_managed_node_groups = {
     example = {
